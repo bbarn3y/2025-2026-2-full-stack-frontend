f4d2f19c3bc9ccb246cfc9ba349b16554069215a
barnabas.balazs <barnabas.balazs@enliven.systems>
2026-03-02 20:09:35 +0100

    2. gyakorlat

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,7 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.less'
+  styleUrl: './app.less',
+  standalone: true,
 })
 export class App {
   protected readonly title = signal('full-stack-2025-2026-2');
